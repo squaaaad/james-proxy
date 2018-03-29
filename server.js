962bc01fac90a341c4c5f33a58640984dbb4afe2
James Davidson <jfdavidson10@gmail.com>
2018-03-28 17:32:51 -0700

    serverside

--- server.js
+++ server.js
@@ -1,32 +1,44 @@
-const express = require('express')
+const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
-const request = require('request');
 
 app.use(morgan('dev'));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.redirect('/restaurants/ChIJUcXYWWGAhYARmjMY2bJAG2s/');
+const clientBundles = './public/services';
+const serverBundles = './templates/services';
+const serviceConfig = require('./service-config.json');
+const services = require('./loader.js')(clientBundles, serverBundles, serviceConfig);
+
+const React = require('react');
+const ReactDom = require('react-dom/server');
+const Layout = require('./templates/layout');
+const App = require('./templates/app');
+const Scripts = require('./templates/scripts');
+
+// see:
+// https://medium.com/styled-components/the-simple-guide-to-server-side-renderin
+// g -react-with-styled-components-d31c6b2b8fbf
+const renderComponents = (components, props = {}) => {
+  return Object
+    .keys(components)
+    .map(item => {
+      let component = React.createElement(components[item], props);
+      return ReactDom.renderToString(component);
+    });
+};
+
+app.get('/restaurants/:id/', (req, res) => {
+  let components = renderComponents(services, {itemid: req.params.id});
+  res.end(Layout('', App(...components), Scripts(Object.keys(services))));
 });
 
-app.use('/restaurants/:id', express.static(path.join(__dirname, 'public')));
-
-app.get('/api/restaurants/:id/gallery', (req, res) => {
-  res.redirect(`http://13.57.148.57/api/restaurants/${req.params.id}/gallery`)
-});
-app.get('/api/restaurants/:id/overview', (req, res) => {
-  res.redirect(`http://184.169.248.150/api/restaurants/${req.params.id}/overview`)
-});
-app.get('/api/restaurants/:id/sidebar', (req, res) => {
-  res.redirect(`http://54.177.233.239/api/restaurants/${req.params.id}/sidebar`)
-});
 app.get('/api/restaurants/:id/recommendations', (req, res) => {
-  res.redirect(`http://52.89.102.101/api/restaurants/${req.params.id}/recommendations`)
+  res.redirect(`http://localhost:3004/api/restaurtants/${req.params.id}/recommendations/`);
 });
 
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`)
+  console.log(`server running at: http://localhost:${port}`);
 });
---
+++ templates/app.js
@@ -0,0 +1,6 @@
+module.exports = (recommendations) => `
+  
+  <div id="recommendations">${recommendations}</div>
+`;
+// <div id="Info">${info}</div>   <div id="CountDown">${countdown}</div>   <div
+// id="Related">${related}</div>
